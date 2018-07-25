b31a8ed775e3e0b4b796d5c9233e7039f50bc1c3
Sami Mäläskä <epsilon@welho.com>
2018-07-25 20:15:48 +0300

    Teht2

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -9,14 +9,22 @@ const Otsikko = (props) => {
    )
 }
 
-const Sisalto = (props) => {
+const Osa = (props) => {
     return (
       <div>
-      <p>{props.osa1} {props.tehtavia1}</p>
-      <p>{props.osa2} {props.tehtavia2}</p>
-      <p>{props.osa3} {props.tehtavia3}</p>
+      <p>{props.osanimi} {props.tehtavia}</p>
       </div>
     ) 
+}
+
+const Sisalto = (props) => {
+  return (
+    <div>
+    <Osa osanimi={props.osa1} tehtavia={props.tehtavia1} />
+    <Osa osanimi={props.osa2} tehtavia={props.tehtavia2} />
+    <Osa osanimi={props.osa3} tehtavia={props.tehtavia3} />
+    </div>
+  ) 
 }
 
 const Yhteensa = (props) => {
