58decb7612360663c383a0e4661ac75c24631e7b
Sami Mäläskä <epsilon@welho.com>
2018-07-25 22:41:40 +0300

    Teht5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Otsikko = (props) => {
    return (
        <div>
-        <h1>{props.kurssi}</h1>
+        <h1>{props.kurssi.nimi}</h1>
         </div>
    )
 }
@@ -20,9 +20,9 @@ const Osa = (props) => {
 const Sisalto = (props) => {
   return (
     <div>
-    <Osa osa={props.osat[0]} />
-    <Osa osa={props.osat[1]} />
-    <Osa osa={props.osat[2]} />
+    <Osa osa={props.kurssi.osat[0]} />
+    <Osa osa={props.kurssi.osat[1]} />
+    <Osa osa={props.kurssi.osat[2]} />
     </div>
   ) 
 }
@@ -40,27 +40,30 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa osat={osat} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa osat={kurssi.osat} />
     </div>
   )
 }
