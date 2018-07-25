5a4f1567be0183487d5b71f6bc04bf6a53fdc192
Sami Mäläskä <epsilon@welho.com>
2018-07-25 23:15:19 +0300

    teht8

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Statistic = ({text,value, posttext}) =>  {
+  return (
+    <div>
+      {text}: {value} {posttext}
+    </div>
+  )
+}
+
+const Statistics = ({hyvat,huonot,neutraalit}) =>  {
+  let maara = hyvat + neutraalit + huonot
+  let keskiarvo = (hyvat-huonot)/maara
+  let positiiviset = hyvat*100/maara
+  return (
+    <div>
+      <h1>statistiikka</h1>
+      <Statistic text="hyvä" value={hyvat} />
+      <Statistic text="neutraali" value={neutraalit} />
+      <Statistic text="huono" value={huonot} />
+      <Statistic text="keskiarvo" value={keskiarvo.toFixed(1)} />
+      <Statistic text="positiivisia" value={positiiviset.toFixed(1)} posttext="%"/>
+    </div>
+  )
+}
+
+
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
       hyva: 0,
       neutraali: 0,
-      huono: 0,
+      huono: 0
     }
 
     this.incHyvat = this.incHyvat.bind(this)
@@ -26,65 +58,33 @@ class App extends React.Component {
     this.setState({huono: this.state.huono + 1})   
   }
  
-
   render() {
-    const Keskiarvo = () => {
-      let maara = this.state.hyva + this.state.neutraali + this.state.huono
-      
-      if (maara===0) return(<div>keskiarvo -</div>)
-
-      return (
-        <div>
-          keskiarvo {((this.state.hyva-this.state.huono)/maara).toFixed(1)}
-        </div>    
-      )
-    }
-
-    const Positiivisia = () => {
-      let maara = this.state.hyva + this.state.neutraali + this.state.huono
-
-      if (maara===0) return(<div>positiivisia - %</div>)
-
-      return (
-        <div>
-          positiivisia {((this.state.hyva*100)/maara).toFixed(1)} %
-        </div>    
-      )
-    }
-
-    const DisplayTila = () => {
-      return (
-        <div>
-          <h1>statistiikka</h1>
-          <div>hyvä {this.state.hyva}</div>
-          <div>neutraali {this.state.neutraali}</div>
-          <div>huono {this.state.huono}</div>
-        </div>
-      )
-    } 
-        
     return (
       <div>
         <h1>anna palautetta</h1>
-        <div>
-          <button onClick={this.incHyvat}>
-            hyva
-          </button>
-          <button onClick={this.incNeutraalit}>
-            neutraali
-          </button>
-          <button onClick={this.incHuonot}>
-            huono
-          </button>
-        </div>
-        <DisplayTila />
-        
-        <Keskiarvo />
-        <Positiivisia />
+        <Button
+          handleClick={this.incHyvat}
+          text="hyva"
+        />
+        <Button
+          handleClick={this.incNeutraalit}
+          text="neutraali"
+        />
+        <Button
+          handleClick={this.incHuonot}
+          text="huono"
+        />
+
+        <Statistics 
+          hyvat={this.state.hyva}
+          huonot={this.state.huono}      
+          neutraalit={this.state.neutraali}
+        /> 
       </div>  
     )
   }
 }
+
 
 ReactDOM.render(
   <App />,
